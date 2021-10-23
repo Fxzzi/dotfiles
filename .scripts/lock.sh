e5d885669ccb924dd7927c0947ca94d5130e2532
Faaris <faaris@Arch.localdomain>
2021-10-23 16:31:04 +0100

    new .scripts

---
+++ .scripts/lock.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ICON=$HOME/.xlock/icon.png
+TMPBG=/tmp/screen.png
+scrot /tmp/screen.png
+convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+i3lock -u -i $TMPBG
+rm /tmp/screen.png
