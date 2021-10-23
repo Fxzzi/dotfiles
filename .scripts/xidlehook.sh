9831ecadc93fb92b633ab87fac8669fb2d191a9a
Faaris <faaris@Arch.localdomain>
2021-10-23 18:41:10 +0100

    replace xautolock with xidlehook

--- .scripts/xidlehook.sh
+++ .scripts/xidlehook.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-# Run xidlehook
 xidlehook \
-  `# Don't lock when there's a fullscreen application` \
   --not-when-fullscreen \
-  `# Don't lock when there's audio playing` \
   --not-when-audio \
-  `# lock the screen after 600 seconds` \
-  --timer 600 \
+  --timer 300 \
     '~/dotfiles/.scripts/lock.sh' \
     '' \
-  `# Finally, suspend 10 minutes after it locks` \
-  --timer 600 \
-    'systemctl suspend"' \
+  --timer 300 \
+    'systemctl suspend' \
     ''
