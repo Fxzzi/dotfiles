d8a63bdb1ea7ee8c5f1d583a2ee3c49d5155f8e2
faaris <faaris@Kappa>
2022-10-22 17:05:24 +0100

    add mpd and ncmpcpp

---
+++ .local/scripts/volume.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+send_notification() {
+	volume=$(pamixer --get-volume)
+    if (( volume >= 70 )); then icon=notification-audio-volume-high
+    elif (( volume >= 40 )); then icon=notification-audio-volume-medium
+    elif (( volume > 0 )); then icon=notification-audio-volume-low
+    else icon=notification-audio-volume-muted
+    fi
+  dunstify -a "changevolume" -u low -r "9993" -h int:value:"$volume" -i "$icon" "Volume: ${volume}%" -t 2000
+}
+
+case $1 in
+up)
+	# Set the volume on (if it was muted)
+	pamixer -u
+	pamixer -i 5
+	send_notification "$1"
+	;;
+down)
+	pamixer -u
+	pamixer -d 5
+	send_notification "$1"
+	;;
+mute)
+	pamixer -t
+	if eval "$(pamixer --get-mute)"; then
+		dunstify -i notification-audio-volume-muted -a "changevolume" -t 2000 -r 9993 -u low "Muted"
+	else
+		send_notification up
+	fi
+	;;
+esac
