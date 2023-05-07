887ca685c87d46623bb109ab6eb45c500b81ac58
faaris <faaris@Kappa>
2023-05-07 15:45:18 +0100

    fix zgenom install directory

--- assets/bootstrap/zgenom.sh
+++ assets/bootstrap/zgenom.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-if [ ! -r $ZDOTDIR/zgenom.zsh ]; then
+if [ ! -r $ZDOTDIR/.zgenom/zgenom.zsh ]; then
   echo "Installing zgenom to: ${ZDOTDIR}..."
-  git clone https://github.com/jandamm/zgenom.git $ZDOTDIR
+  git clone https://github.com/jandamm/zgenom.git $ZDOTDIR/.zgenom
 else
   echo "zgenom is already installed"
 fi
