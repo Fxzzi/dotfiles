bb25e14fa10eb538996d613feeab3bbb850365d9
Fazzi <faaris.ansari@proton.me>
2023-08-17 15:08:13 +0100

    update hyprland blur config to new subcategory, remove usage of waybar-hyprland and use wayabr with the hyprland/workspaces module instead, remove GBM_BACKEND envvar as it is now causing issues

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -25,6 +25,7 @@ alias gd='git diff'
 alias gp='git push'
 
 # Misc aliases
-alias updates='checkupdates; paru -Qum'  # Check system and AUR updates
+alias updates='checkupdates; paru -Qum; pkill -RTMIN+1 waybar'  # Check system and AUR updates
 alias rs='curl --data-binary @- https://paste.rs | wl-copy'  # Share output to a pastebin and copy to clipboard
 alias vim='nvim' # Editor
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
