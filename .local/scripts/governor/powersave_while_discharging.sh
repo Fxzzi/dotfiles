097a4dcfc5c7da98f439ae9d498732a81b30966a
Fazzi <faaris.ansari@proton.me>
2023-10-15 00:41:45 +0100

    update dunst config, rounding n stuff

--- .local/scripts/governor/powersave_while_discharging.sh
+++ .local/scripts/governor/powersave_while_discharging.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 echo "powersave" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-echo "power" | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
+echo "balance_power" | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
 
 pkill -SIGUSR2 waybar
