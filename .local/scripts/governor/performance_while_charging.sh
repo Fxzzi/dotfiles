a4b215b1402e45e6228a63a50f9e3f781eb41583
Fazzi <faaris.ansari@proton.me>
2023-10-15 10:03:57 +0100

    remove lazyvim from install.conf.yaml, add sleep in governor scripts to avoid race

--- .local/scripts/governor/performance_while_charging.sh
+++ .local/scripts/governor/performance_while_charging.sh
@@ -3,4 +3,5 @@
 echo "performance" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 echo "performance" | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
 
+sleep 1
 pkill -SIGUSR2 waybar
--- .local/scripts/governor/powersave_while_discharging.sh
+++ .local/scripts/governor/powersave_while_discharging.sh
@@ -3,4 +3,5 @@
 echo "powersave" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 echo "balance_power" | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
 
+sleep 1
 pkill -SIGUSR2 waybar
