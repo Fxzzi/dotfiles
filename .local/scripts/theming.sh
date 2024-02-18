5bfc470bce63ac7fdaf600d05c4a90227ea9da0d
Fazzi <faaris.ansari@proton.me>
2024-02-18 22:50:17 +0000

    update hyprland device syntax for new versions, edit waybar css, change cursor theme

--- .local/scripts/theming.sh
+++ .local/scripts/theming.sh
@@ -5,9 +5,9 @@ gsettings set org.gnome.desktop.interface gtk-theme TokyoNight
 # Set the icon theme to Papirus-Dark
 gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
 # Set the default font and size
-gsettings set org.gnome.desktop.interface font-name 'SF Pro Text 12'
+gsettings set org.gnome.desktop.interface font-name 'Noto Sans 12'
 # Set the cursor theme and size
-gsettings set org.gnome.desktop.interface cursor-theme Simp1e-Tokyo-Night
+gsettings set org.gnome.desktop.interface cursor-theme Simp1e-Tokyo-Night-Light
 gsettings set org.gnome.desktop.interface cursor-size 24
 # Set font antialiasing
 gsettings set org.gnome.desktop.interface font-antialiasing rgba
@@ -16,4 +16,4 @@ gsettings set org.gnome.desktop.interface font-hinting slight
 # Set preferred theme to dark mode
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 # Set hyprland cursor
-hyprctl setcursor Simp1e-Tokyo-Night 24
+hyprctl setcursor Simp1e-Tokyo-Night-Light 24
