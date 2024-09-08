ffb9c4ba7377bc8742235380683e45e04c328ff0
Fazzi <faaris.ansari@proton.me>
2024-09-08 11:48:35 +0100

    update waybar styling, minor changes

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -1,33 +1,20 @@
 return {
 	{
 		"folke/which-key.nvim",
-		-- event = "VeryLazy",
-		keys = {"<leader>"},
-		config = function()
-			local wk = require("which-key")
-			wk.setup({
-				key_labels = {
-					["<space>"] = "SPC",
-					["<leader>"] = "SPC",
-					["<cr>"] = "RET",
-					["<tab>"] = "TAB",
-				},
-				icons = {
-					breadcrumb = "",
-					group = " ",
-				},
-			})
-
-			wk.register({
-				["<leader>l"] = { name = "Line" },
-				["<leader>w"] = { name = "Workspace" },
-				["<leader>b"] = { name = "Buffer" },
-				["<leader>s"] = { name = "Symbol" },
-				["<leader>a"] = { name = "Add" },
-				["<leader>d"] = { name = "Remove" },
-				["<leader>o"] = { name = "Open" },
-				["<leader>f"] = { name = "Find" },
-			})
-		end,
-	},
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	}
 }
