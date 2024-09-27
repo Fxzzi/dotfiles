eb42f256ba2a97dcf395338679a769c4d16f943a
Fazzi <faaris.ansari@proton.me>
2024-09-27 19:19:54 +0100

    help me mr wumpus

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,6 +1,8 @@
 import App from "resource:///com/github/Aylur/ags/app.js";
 import Gdk from "gi://Gdk";
+import { idle } from "resource:///com/github/Aylur/ags/utils/timeout.js";
 
+// Import widget modules
 import { DateWidget } from "./modules/Date.js";
 import { TimeWidget } from "./modules/Time.js";
 import { BatteryWidget } from "./modules/Battery.js";
@@ -13,6 +15,13 @@ import { MemUsageWidget } from "./modules/MemUsage.js";
 import { GpuTempWidget } from "./modules/GpuTemp.js";
 import { SysTrayWidget } from "./modules/SysTray.js";
 
+// Define per-monitor windows (bars, popups, etc.)
+const perMonitorWindows = [Bar];
+
+/* Define unique windows (no need to be created per monitor) */
+const uniqueWindows = [];
+
+// Left section of the bar
 function Left(monitor) {
   return Widget.Box({
     spacing: 10,
@@ -25,6 +34,7 @@ function Left(monitor) {
   });
 }
 
+// Center section of the bar
 function Center() {
   return Widget.Box({
     spacing: 10,
@@ -32,13 +42,14 @@ function Center() {
   });
 }
 
+// Right section of the bar
 function Right() {
   return Widget.Box({
     hpack: "end",
     spacing: 10,
     children: [
       SysTrayWidget(),
-			GpuTempWidget(),
+      GpuTempWidget(),
       CpuTempWidget(),
       CpuUsageWidget(),
       MemUsageWidget(),
@@ -47,12 +58,12 @@ function Right() {
   });
 }
 
-// Bar layout
-function Bar(monitor = 0) {
+// Bar layout for a specific monitor
+function Bar(monitor) {
   return Widget.Window({
     name: `bar-${monitor}`,
     class_name: "bar",
-    monitor,
+    gdkmonitor,
     anchor: ["top", "left", "right"],
     exclusivity: "exclusive",
     child: Widget.CenterBox({
@@ -63,33 +74,47 @@ function Bar(monitor = 0) {
   });
 }
 
+// Run idle to set up initial windows and monitor events
+idle(async () => {
+  // Add unique windows (widgets that don't depend on specific monitors)
+  uniqueWindows.map((win) => App.addWindow(win));
+
+  // Get the default display and create bars for each monitor
+  const display = Gdk.Display.get_default();
+  if (display) {
+    for (let m = 0; m < display.get_n_monitors(); m++) {
+      const monitor = display.get_monitor(m);
+      perMonitorWindows.map((win) => App.addWindow(win(monitor)));
+    }
+
+    // Monitor added event
+    display.connect("monitor-added", (disp, monitor) => {
+      perMonitorWindows.map((win) => App.addWindow(win(monitor)));
+    });
+
+    // Monitor removed event
+    display.connect("monitor-removed", (disp, monitor) => {
+      App.windows.forEach((window) => {
+        if (window.gdkmonitor === monitor) App.removeWindow(window);
+      });
+    });
+  }
+});
+
+// App configuration
 App.config({
   style: "./style.css",
 });
 
-// add svg icons from icons directory
+// Add SVG icons from the icons directory
 App.addIcons(`${App.configDir}/icons/`);
 
-// reload css when wallust updates colors
+// Reload CSS when wallust updates colors
 Utils.subprocess(
   ["inotifywait", "--event", "modify", "-m", "-q", `${App.configDir}`],
   () => {
-    console.log("Caught wallust reload, reloading css!");
+    console.log("Caught wallust reload, reloading CSS!");
     App.resetCss();
     App.applyCss(`${App.configDir}/style.css`);
   },
 );
-
-const range = (length, start = 1) =>
-  Array.from({ length }, (_, i) => i + start);
-
-// get number of monitors and create a bar for each
-function forMonitorsAsync(widget) {
-  const display = Gdk.Display.get_default();
-  const n = display ? display.get_n_monitors() : 1; // Number of monitors
-  return range(n, 0).forEach((monitor) => {
-    widget(monitor);
-  });
-}
-
-forMonitorsAsync(Bar);
