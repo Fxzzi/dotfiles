a52785ad876f18006c26df1e08e0af7b4bae564f
Fazzi <faaris.ansari@proton.me>
2024-10-22 18:37:48 +0100

    minor changes, change some fonts to just use monospace

--- .local/scripts/gsettings.sh
+++ .local/scripts/gsettings.sh
@@ -2,12 +2,16 @@
 
 # Set the GTK theme to TokyoNight
 gsettings set org.gnome.desktop.interface gtk-theme TokyoNight
+
 # Set the icon theme to Papirus-Dark
 gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
+
 # Set the default font and size
 gsettings set org.gnome.desktop.interface font-name 'Noto Sans 12'
+
 # Set the cursor theme and size
 gsettings set org.gnome.desktop.interface cursor-theme DeepinV20-white
 gsettings set org.gnome.desktop.interface cursor-size 24
+
 # Set preferred theme to dark mode
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
